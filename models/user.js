11efa85600a4aa0c53da2812185a54948f290d62
Reeka <reekamaharaj@gmail.com>
2020-05-09 11:10:32 -0500

    password hashing, haven't tested yet

--- models/user.js
+++ models/user.js
@@ -1,16 +1,36 @@
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
+        id: {
+            primarayKey: true,
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true
+        },
         username: {
             type: DataTypes.STRING,
-            allowNull: false
+            is: ["^[a-z]+$", "i"],
+            allowNull: false,
+            required: true
         },
         password: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            required: true,
+            len: [2, 10]
         }
     }, {
         timestamps: false
     }
     );
     return User;
+};
+
+//generate hash
+User.generateHash = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.localPassword);
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,9 @@
 
 // Require models
 var db = require("../models");
+var bcrypt = require("bcrypt");
+const saltRounds = 10;
+//salt round => cost factor controls how much time is needed to calculate a single bCrypt hash. higher cost factor -> more hashing rounds. Salt is a random value, store hash-string, also stores the saltn no less that 12 for production ready code. 
 
 // Routes
 module.exports = function(app) {
@@ -12,13 +15,39 @@ module.exports = function(app) {
         });
     });
 
-    // Create a new user
-    app.post("/api/users", function(req, res) {
-        db.User.create({
-            username: req.body.username,
-            password: req.body.password
-        }).then(function(dbUser) {
-            res.json(dbUser);
+    //Login page
+    app.post("/login", function(req, res) {
+        db.User.findOne({
+            where: {
+                username: req.body.username
+            }
+        }).then(function(user){
+            if (!user){
+                res.redirect("/");
+            } else {
+                bcrypt.compare(req.body.password, user.password, function (err, result) {
+                    if (result == true) {
+                        res.redirect("/");
+                    } else {
+                        res.send("Incorrect password");
+                        res.redirect("/");
+                    }
+                });
+            }
         });
+    });
+
+    // Create a new user
+    app.post("/register", function(req, res) {
+        bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+            db.User.create({
+                username: req.body.username,
+                password: hash
+            }).then(function(data) {
+                if (data) {
+                    res.redirect("/");
+                }
+            });
+        })
     });
 };
