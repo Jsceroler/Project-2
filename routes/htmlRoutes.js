25c040db4f03424317d3392d415dad2c7650b4c3
Reeka <reekamaharaj@gmail.com>
2020-05-11 16:10:54 -0500

    api is being searched, just need to fix the display to the page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -28,62 +28,14 @@ module.exports = function(app) {
         res.render("favs");
     });
 
-    //from starter code
-    // db.User.findOne({ where: { id: req.params.id } }).then(function(
-    //     dbUser
-    // ) {
-    //     res.render("detail", {
-    //         msg: "detail page",
-    //         user: dbUser
-    //     });
-    // });
-
     app.post("/", function(req, res) {
-        const validZip = /^\d{5}$/;
-
-        let token;
-        //get the token first
-        fetch("https://api.petfinder.com/v2/oauth2/token", {
-            body: `grant_type=client_credentials&client_id=${process.env.KEY}&client_secret=${process.env.SECRET}`,
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            method: "POST",
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                token = data.access_token;
-            });
-
-        function showAnimals(animalInfo) {
-            animalInfo.forEach((animal) => {
-                let image;
-                if (animal.photos.length > 0) {
-                    image = animal.photos[0].medium;
-                } else {
-                    image =
-                        "<a href='https://placeholder.com'><img src='https://via.placeholder.com/150'></a>";
-                }
-            });
-        }
-
-        // Fetch animals from the API
-        function fetchAnimals(animal, zip) {
-            // fetch pets
-            // get data using the token
-            fetch(
-                `https://api.petfinder.com/v2/animals/?type=${animal}&contact.address.postcode=${zip}`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                }
-            )
-                .then((response) => response.json())
-                .then((data) => {
-                    showAnimals(data.animals);
-                });
-        }
+        let animalObj = {};
+        let animalSearch = {
+            animal: req.body.animal,
+            zip: req.body.zip
+        };
+        apiFetch(animalSearch);
+        res.render("index", animalObj);
     });
 
     // Render 404 page for any unmatched routes
@@ -91,3 +43,39 @@ module.exports = function(app) {
         res.render("404");
     });
 };
+
+function apiFetch(searchParams){
+    let token;
+    //get the token first
+    fetch("https://api.petfinder.com/v2/oauth2/token", {
+        body: `grant_type=client_credentials&client_id=${process.env.KEY}&client_secret=${process.env.SECRET}`,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        method: "POST",
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            token = data.access_token;
+            fetchAnimals(searchParams, token);
+        });
+}
+
+// Fetch animals from the API
+function fetchAnimals(params, token) {
+    // fetch pets
+    // get data using the token
+    fetch(
+        `https://api.petfinder.com/v2/animals/?type=${params.animal}&contact.address.postcode=${params.zip}`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            animalObj = {
+                data
+            };
+        });
+}
