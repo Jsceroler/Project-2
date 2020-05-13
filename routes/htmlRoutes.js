a5210cb786e302067c6f78d9211e1fc82b2bd2b0
asalinas9 <salinas99@live.com>
2020-05-12 19:41:06 -0500

    api works with handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,11 @@ const db = require("../models");
 const fetch = require("node-fetch");
 require("dotenv").config();
 
+console.log(process.env);
+
 module.exports = function (app) {
     // Load index page
-    app.get("/", function(req, res) {
+    app.get("/", function (req, res) {
         res.render("index");
     });
 
@@ -28,14 +30,17 @@ module.exports = function (app) {
         res.render("favs");
     });
 
-    app.post("/", function(req, res) {
-        let animalObj = {};
+    app.post("/", function (req, res) {
+
         let animalSearch = {
             animal: req.body.animal,
             zip: req.body.zip
         };
-        apiFetch(animalSearch);
-        res.render("index", animalObj);
+        apiFetch(animalSearch).then((animalObj) => {
+
+            res.render("index", { animalObj })
+        });
+
     });
 
     // Render 404 page for any unmatched routes
@@ -44,10 +49,11 @@ module.exports = function (app) {
     });
 }
 
-function apiFetch(searchParams){
+function apiFetch(searchParams) {
+
     let token;
     //get the token first
-    fetch("https://api.petfinder.com/v2/oauth2/token", {
+    return fetch("https://api.petfinder.com/v2/oauth2/token", {
         body: `grant_type=client_credentials&client_id=${process.env.KEY}&client_secret=${process.env.SECRET}`,
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
@@ -57,7 +63,7 @@ function apiFetch(searchParams){
         .then((response) => response.json())
         .then((data) => {
             token = data.access_token;
-            fetchAnimals(searchParams, token);
+            return fetchAnimals(searchParams, token);
         });
 }
 
@@ -65,8 +71,8 @@ function apiFetch(searchParams){
 function fetchAnimals(params, token) {
     // fetch pets
     // get data using the token
-    fetch(
-        `https://api.petfinder.com/v2/animals/?type=${params.animal}&contact.address.postcode=${params.zip}`,
+    return fetch(
+        `https://api.petfinder.com/v2/animals/?type=${params.animal}&location=${params.zip}`,
         {
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -74,9 +80,8 @@ function fetchAnimals(params, token) {
         })
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
-            return animalObj = {
-                animals: data
-            };
+            //   console.log(data);
+            return data
+
         });
 }
