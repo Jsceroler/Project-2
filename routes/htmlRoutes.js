8942840fba9926cc793f2bcdd10aed946a277aa9
Reeka <reekamaharaj@gmail.com>
2020-05-13 15:31:48 -0500

    working on sessions and expanding the search params

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,67 +4,68 @@ require("dotenv").config();
 
 console.log(process.env);
 
-module.exports = function (app) {
+module.exports = function(app) {
     // Load index page
-    app.get("/", function (req, res) {
+    app.get("/", function(req, res) {
         res.render("index");
     });
 
     // Load login page
-    app.get("/login", function (req, res) {
+    app.get("/login", function(req, res) {
         if (req.session.username) {
-            console.log("Already logged in");
-            res.render("favs");
+            res.render("Logged in as " + req.session.username);
         } else {
-            res.render("login")
+            res.render("login");
         }
     });
 
-    // Load register page 
-    app.get("/register", function (req, res) {
+    // Load register page
+    app.get("/register", function(req, res) {
         if (req.session.username) {
-            console.log("already logged in");
+            res.render("Logged in as " + req.session.username);
         } else {
             res.render("register");
         }
     });
 
-    // Load detail page 
-    app.get("/detail", function (req, res) {
-        res.render("detail");
-    });
+    // Load detail page --Leaving this commented out since we probably won't use this--
+    // app.get("/detail", function (req, res) {
+    //     res.render("detail");
+    // });
 
-    // Load fav page 
-    app.get("/favs", function (req, res) {
+    // Load fav page
+    app.get("/favs", function(req, res) {
         if (req.session.username) {
             res.render("favs");
-        }
-        else {
+        } else {
             res.render("login");
         }
     });
 
-    app.post("/", function (req, res) {
-
+    app.post("/", function(req, res) {
         let animalSearch = {
             animal: req.body.animal,
-            zip: req.body.zip
+            size: req.body.size,
+            gender: req.body.gender,
+            age: req.body.age,
+            coat: req.body.coat,
+            good_with_children: req.body.good_with_children,
+            good_with_dogs: req.body.good_with_dogs,
+            good_with_cats: req.body.good_with_cats,
+            zip: req.body.zip,
         };
         apiFetch(animalSearch).then((animalObj) => {
-
-            res.render("index", { animalObj })
+            res.render("index", { animalObj });
         });
-
     });
 
     // Render 404 page for any unmatched routes
-    app.get("*", function (req, res) {
+    app.get("*", function(req, res) {
         res.render("404");
     });
-}
+};
 
 function apiFetch(searchParams) {
-
     let token;
     //get the token first
     return fetch("https://api.petfinder.com/v2/oauth2/token", {
@@ -86,16 +87,15 @@ function fetchAnimals(params, token) {
     // fetch pets
     // get data using the token
     return fetch(
-        `https://api.petfinder.com/v2/animals/?type=${params.animal}&location=${params.zip}`,
+        `https://api.petfinder.com/v2/animals/?type=${params.animal}&size=${params.size}&gender=${params.gender}&age=${params.age}&coat=${params.coat}&good_with_children=${params.good_with_children}&good_with_dogs=${params.good_with_dogs}&good_with_cats=${params.good_with_cats}&location=${params.zip}`,
         {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
-        })
+        }
+    )
         .then((response) => response.json())
         .then((data) => {
-            //   console.log(data);
-            return data
-
+            return data;
         });
 }
