17da857c88ce40d0daf21851086f03ff2a7732b4
asalinas9 <salinas99@live.com>
2020-05-15 10:08:39 -0500

    added default pic

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,30 +3,30 @@ const fetch = require("node-fetch");
 const qs = require("qs");
 require("dotenv").config();
 
-module.exports = function(app) {
+module.exports = function (app) {
     // Load index page
-    app.get("/", function(req, res) {
+    app.get("/", function (req, res) {
         res.render("index");
     });
 
     // Load login page
     app.get("/login", function (req, res) {
-            if (req.session.username) {
-                res.render("index", {
-                    message: "Logged in as: ",
-                    username: req.session.username
-                });
-            } else {
-                res.render("login", {message: "Please enter your username and password to login."});
-            }
-        });
+        if (req.session.username) {
+            res.render("index", {
+                message: "Logged in as: ",
+                username: req.session.username
+            });
+        } else {
+            res.render("login", { message: "Please enter your username and password to login." });
+        }
+    });
 
-// Load register page 
+    // Load register page 
     app.get("/register", function (req, res) {
         if (req.session.username) {
-            res.render("index", {message: "You are already logged in! If you want to create a new account please log out first."})
+            res.render("index", { message: "You are already logged in! If you want to create a new account please log out first." })
         } else {
-            res.render("register", {message: "Please enter a username and password you would like to use"});
+            res.render("register", { message: "Please enter a username and password you would like to use" });
         }
     });
 
@@ -35,21 +35,22 @@ module.exports = function(app) {
     //     res.render("detail");
     // });
 
-// Load fav page 
+    // Load fav page 
     app.get("/favs", function (req, res) {
         if (req.session.username) {
-            res.render("favs", { 
+            res.render("favs", {
                 usernameDisplay: "You are logged in as: " + req.session.username,
-                message: "Display the saved favs, or something saying no favs have been saved."});
+                message: "Display the saved favs, or something saying no favs have been saved."
+            });
         }
         else {
-            res.render("favs", {message: "You are not logged in, if you would like to look at favs please log in"});
+            res.render("favs", { message: "You are not logged in, if you would like to look at favs please log in" });
         }
     });
 
     //multiple options: size, gender, age, coat
 
-    app.post("/", function(req, res) {
+    app.post("/", function (req, res) {
         let animalSearch = {
             type: req.body.animal,
             // key value set up for the other search params
@@ -69,7 +70,7 @@ module.exports = function(app) {
     });
 
     // Render 404 page for any unmatched routes
-    app.get("*", function(req, res) {
+    app.get("*", function (req, res) {
         res.render("404");
     });
 };
@@ -98,7 +99,7 @@ function fetchAnimals(params, token) {
     const query = qs.stringify(params);
     console.log(query);
     return fetch(
-        `https://api.petfinder.com/v2/animals/?${query}`,
+        `https://api.petfinder.com/v2/animals/?${query}&limit=6`,
         // search query URL that will use all the params
         {
             headers: {
@@ -109,13 +110,14 @@ function fetchAnimals(params, token) {
         .then((response) => response.json());
 }
 
-function valueCheck(value){
-    if(typeof(value)==="object"){
-        value = Object.keys(value).map(function(k){return value[k]}).join(",");
+function valueCheck(value) {
+    if (typeof (value) === "object") {
+        value = Object.keys(value).map(function (k) { return value[k] }).join(",");
         return value;
     }
-    else{
+    else {
         return value;
     }
 }
+
 
